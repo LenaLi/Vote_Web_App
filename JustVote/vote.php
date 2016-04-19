7fcc520f7658e2ac3d6d1e1bd570eed590274c45
LenaLi <ll033@hdm-stuttgart.de>
2016-04-19 21:37:22 +0200

    VOTE Link Seite

--- JustVote/vote.php
+++ JustVote/vote.php
@@ -1,3 +1,5 @@
+<!-- Hier kommt Link + QR Code zum jeweiligen Voting hinein -->
+
 <?php include("inc/session_check.php"); ?>
 
 <!DOCTYPE html>
@@ -18,14 +20,48 @@
                 <div class="col-lg-12">
                     <h1>Ihre Vorlesungen und Votings</h1>
                     <p></p>
-
                     <?php
-                    // DB Abfrage zum Voting
+                    // DB Abfrage zu Vorlesungen von Benutzer.....
+                    require_once("Mapper/vorlesung.php");
+                    require_once("Mapper/vorlesung_manager.php");
                     require_once("Mapper/voting_manager.php");
+                    //DB Abfrage zu Votings
 
-                    $vorlesungen = $votingmanager->findByVotingId($_SESSION['votingid']);
+                    $votingmanager =new voting_manager();
+                    $votings = $votingmanager->findByVotingId($_SESSION['votingid']);
+                    $votingmanager =new voting_manager();
+                    $aktuellesvoting=$_GET['id'];
 
-                    $votingmanager =new voting_manager(); ?>
+                    while ($aktuellesvoting=$votings)
+                        foreach($votings as $voting){
+                            echo "<table class='table table-hover'>";
+                            // Frage und Antwortmöglichkeiten
+                            echo "<thead><tr>";
+                            echo "<th colspan='12'>" . $voting->frage ;
+                            echo "<th>" . $voting->antwort_1 ;
+                            echo "<th>" . $voting->antwort_2 ;
+                            echo "<th>" . $voting->antwort_3 ;
+                            echo "<th>" . $voting->antwort_4 ;
+
+
+                            echo  "<th>";
+                            echo "</tr> </thead>";
+
+                            $votings=$votingmanager->findByVorlesungsId($vorlesung->vorlesungsid);
+
+                            foreach($votings as $voting){
+                                echo "<tr>";
+                                echo "<th>" . $voting->votingname . "</th>";
+
+
+
+
+
+                                echo "</tr>";
+                            }
+                            echo "</table>";
+                        }
+                    ?>
 
 
 
