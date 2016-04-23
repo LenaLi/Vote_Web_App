286f48763db2ab08fdc14ddcf77889a926a2cb2a
rs095 <r.schwaighofer@hotmail.com>
2016-04-23 15:16:58 +0200

    student create do

--- JustVote/student_create_do.php
+++ JustVote/student_create_do.php
@@ -1,7 +1,24 @@
+<?php include("inc/session_check.php");
+
+require_once("Mapper/student.php");
+require_once("Mapper/student_manager.php");
+
+?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: lenalidl
- * Date: 21.04.16
- * Time: 11:18
- */
+
+//POST Parameter (vorname, nachname, email,)
+$vorname=$_POST["vorname"];
+$nachname=$_POST["nachname"];
+$email=$_POST["email"];
+$role=$_POST["role"];
+//Datenbankverbindung aufbauen
+
+$manager=new student_manager();
+
+$manager->create($vorname,$nachname,$email,$role);
+
+
+//Weiterleitung auf benutzer_read oder Fehlermeldung (keine Dopplungen, keine Berechtigungen)
+header('Location: student_read.php');
+?>
