28fe83736bea63334e464b3ffaf0340dba747be5
LenaLi <ll033@hdm-stuttgart.de>
2016-04-23 13:16:55 +0200

    NEW CSS

--- JustVote/inc/header.php
+++ JustVote/inc/header.php
@@ -3,9 +3,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/simple-sidebar.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
     <link rel="stylesheet" href="css/sb-admin.css">
     <link rel="stylesheet" href="css/sb-admin-rtl.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <script src="js/jquery-1.11.3.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
