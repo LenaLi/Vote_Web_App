b34423379d6cf075ea505c27c97c11f62fe2a797
rs095 <r.schwaighofer@hotmail.com>
2016-04-24 21:04:35 +0200

    vote_student_form

---
+++ JustVote/vote_student_form.php
@@ -0,0 +1,14 @@
+<?php include("inc/session_check.php"); ?>
+<?php include("inc/header.php"); ?>
+<?php include("inc/navigation.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="vote_student_do.php" method="get">
+    <table>
+
+    </table>
+    </form>
+</body>
+</html>
