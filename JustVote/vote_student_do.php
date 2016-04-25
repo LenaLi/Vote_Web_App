176b10e8234d018d1a9047b13c463d85550dd2a1
rs095 <r.schwaighofer@hotmail.com>
2016-04-25 19:37:26 +0200

    vote_student_form

--- JustVote/vote_student_do.php
+++ JustVote/vote_student_do.php
@@ -11,4 +11,15 @@ $manager=new voting_manager();
 $manager->inputresult($voting_id, $ergebnis);
 ?>
 
-<!-- TODO function beziehungvotingstudent noch anlegen wenn das mit dem einloggen von Studenten geklärt ist!
+<!-- TODO function beziehungvotingstudent noch anlegen wenn das mit dem einloggen von Studenten geklärt ist! -->
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Danke f&uuml;rs Abstimmen!</h1>
+<form action="ergebnis.php" method="post">
+<input type="submit" name="Zu den Ergebnissen" value="Zu den Ergebnissen"/>
+
+</body>
+
+</html>
