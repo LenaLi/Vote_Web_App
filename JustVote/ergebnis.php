538ca67501c523963a32da34d1f4e78f5fa40f3d
rs095 <r.schwaighofer@hotmail.com>
2016-04-25 20:17:06 +0200

    vote_student_form

--- JustVote/ergebnis.php
+++ JustVote/ergebnis.php
@@ -1,1 +1,22 @@
+<?php include ("inc/session_check.php");?>
+<?php include ("inc/header.php");?>
+<?php include ("inc/navigation.php");?>
+
 <?php
+
+public static $dsn = "mysql:dbhost=mars.iuk.hdm-stuttgart.de;dbname=u-rs095";
+public static $dbuser = "rs095";
+public static $dbpass = "mae5zie6Ai";
+
+$link=mysql_connect($dsn, $dbuser, $dbpass);
+
+// or die ('Verbindungsversuch fehlgeschlagen: '.mysql.error($linkerror));
+
+//$sqlstring='SELECT COUNT(ergebnis) AS anzahl FROM ergebnis
+$sqlstring='SELECT * FROM ergebnis';
+$result=mysql_querry($sqlstring);
+
+
+?>
+
+
