09be15ee4caca84ee14ceaf579ace4fe19b4a8fb
cm102 <cm102@hdm-stuttgart.de>
2016-04-28 15:11:19 +0200

    Kommentare

--- JustVote/voting_create_form.php
+++ JustVote/voting_create_form.php
@@ -29,21 +29,21 @@ $vorlesungen=$vorlesungsmanager->findByBenutzerID($benutzerId);
                 <div class="col-lg-12">
 
                     <h2>Neues Voting</h2>
-
+                    <!-- Beginn Formular "Voting hinzufügen" -->
                     <form class="form-horizontal" role="form" action="voting_create_do.php" method="post">
-                        
 
+                        <!-- Texteingabefeld für Name des Votings-->
                         <div class="form-group">
                             <div class="col-sm-6">
                                 <input type="text" class="form-control" name="votingname" id="votingname" placeholder="Name des Votings">
                             </div>
                         </div>
-
+                        <!-- Vorlesung auswählen: -->
                         <div class="form-group">
                             <div class="col-sm-3">
                             <label>Vorlesung auswählen: </label>
                             </div>
-
+                            <!-- Auswahlfeld der Vorlesung-->
                             <div class="col-sm-3">
                         <select class="c-select" name="vorlesungsid">
                             <?php
@@ -58,9 +58,7 @@ $vorlesungen=$vorlesungsmanager->findByBenutzerID($benutzerId);
                             </div>
 
                         </div>
-
-
-
+                        <!-- Texteingabefeld für die Frage des Votings -->
                         <div class="form-group">
                             <div class="col-sm-6"
                             <label for="frage"></label>
@@ -68,35 +66,35 @@ $vorlesungen=$vorlesungsmanager->findByBenutzerID($benutzerId);
                             </div>
                         </div>
 
-
+                        <!-- Texteingabefeld für die Antwortmöglichkeit 1 -->
                          <div class="form-group">
                              <div class="col-sm-6"
                              <label for="antwort_1"></label>
                              <textarea class="form-control" name="antwort_1" rows="2" placeholder="Antwortmöglichkeit 1"></textarea>
                             </div>
                          </div>
-
+                        <!-- Texteingabefeld für die Antwortmöglichkeit 2 -->
                         <div class="form-group">
                             <div class="col-sm-6"
                             <label for="antwort_2"</label>
                             <textarea class="form-control" name="antwort_2" rows="2" placeholder="Antwortmöglichkeit 2"></textarea>
                             </div>
                         </div>
-
+                        <!-- Texteingabefeld für die Antwortmöglichkeit 3 -->
                         <div class="form-group">
                             <div class="col-sm-6"
                             <label for="antwort_3"</label>
                             <textarea class="form-control" name="antwort_3" rows="2" placeholder="Antwortmöglichkeit 3"></textarea>
                             </div>
                         </div>
-
+                        <!-- Texteingabefeld für die Antwortmöglichkeit 4 -->
                         <div class="form-group">
                              <div class="col-sm-6"
                             <label for="antwort_4"</label>
                             <textarea class="form-control" name="antwort_4" rows="2" placeholder="Antwortmöglichkeit 4"></textarea>
                         </div>
 
-
+                        <!-- Eingabefeld für die Startdatum, Startzeit -->
                         <div class="form-group">
                             <div class="col-sm-3"
                             <label for="startdatum"></label>
@@ -104,13 +102,14 @@ $vorlesungen=$vorlesungsmanager->findByBenutzerID($benutzerId);
                             <input class="form-control" type="time" name="startzeit" placeholder="Startzeit">
                         </div>
 
+                        <!-- Eingabefeld für die Enddatum, Endzeit -->
                         <div class="form-group">
                             <div class="col-sm-3"
                             <label for="enddatum"></label>
                             <input class="form-control" type="date" name="enddatum" placeholder="Enddatum">
                             <input class="form-control" type="time" name="endzeit" placeholder="Endzeit">
                         </div>
-
+                        <!-- Button "Erstellen"-->
                             </div>
                             <div class="form-group">
                             <div class=" col-sm-6">
@@ -118,11 +117,10 @@ $vorlesungen=$vorlesungsmanager->findByBenutzerID($benutzerId);
                             </div>
                         </div>
                     </form>
-
+                    <!-- Ende Formular "Voting hinzufügen" -->
                 </div>
             </div>
         </div>
-
 </div>
 
 </body>
