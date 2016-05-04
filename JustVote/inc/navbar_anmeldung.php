3d52ac7979ea4d5f05d39d5293406fd63a85ec03
LenaLi <ll033@hdm-stuttgart.de>
2016-05-05 00:51:34 +0200

    Login für Studenten

---
+++ JustVote/inc/navbar_anmeldung.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenalidl
+ * Date: 04.05.16
+ * Time: 19:04
+ */
--- JustVote/student_create_form.php
+++ JustVote/student_create_form.php
@@ -5,9 +5,13 @@
 
 <?php include("inc/header.php"); ?>
 
-<body>
+<body class="mitte">
 
-<?php include("inc/navbar_vote.php"); ?>
+<!-- LOGO -->
+<div class="mitte">
+    <img src="http://mars.iuk.hdm-stuttgart.de/~ll033/pics/Logo_JustVote.svg" />
+</div>
+
 
 <div id="page-wrapper">
 
@@ -15,7 +19,8 @@
         <div class="row">
             <div class="col-lg-12">
 
-                <h2>Student hinzuf&uuml;gen</h2>
+                <h2>Anmeldung für Studenten</h2>
+                <h5>der Hochschule der Medien Stuttgart mit dem Hochschul-Account</h5>
 
                 <form class="form-horizontal" role="form" action="benutzer_create_do.php" method="post">
 
@@ -36,15 +41,25 @@
                             <input type="text" class="form-control" name="email" id="email" placeholder="E-Mail">
                         </div>
                     </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="password">Kennwort:</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="password" id="password">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="password2">Kennwort (Wiederholung):</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="password2" id="password2">
+                        </div>
+                    </div>
+
                     <div class="form-group">
                         <div class="col-sm-6">
                             <select name="role">
 
-
-
-
-                                <!-- <option value="Benutzer" selected>Benutzer</option>    Das auswählen von Benutzer und Admin sollte ja nicht m�glich sein wenn man einen Studentetn anlegt
-                                <option value="Admin">Admin</option> -->
                                 <option value="Student">Student</option>
                             </select>
                         </div>
@@ -52,7 +67,7 @@
 
                     <div class="form-group">
                         <div class=" col-sm-6">
-                            <button type="submit" class="btn btn-warning"> Student hinzuf&uuml;gen</button>
+                            <button type="submit" class="btn btn-warning"> Anmelden</button>
                         </div>
                     </div>
 
