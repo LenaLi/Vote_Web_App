717cd8e86382d924fc2b07a143c7a332762be8fc
rs095 <r.schwaighofer@hotmail.com>
2016-05-06 12:37:14 +0200

    vote_student_form

--- JustVote/Mapper/voting_student.php
+++ JustVote/Mapper/voting_student.php
@@ -1,5 +1,16 @@
 <?php
 
 class voting_student
+{
+    public $votingstudent_id;
+    public $votingid;
+    public $ergebnis_id
 
-//public $
+    function __construct($data=null) {
+        if (is_array($data)) {
+            $this->votingstudent_id = $data['votingstudent_id'];
+            $this->votingid = $data['votingid'];
+            $this->ergebnis_id = $data['ergebnis_id'];
+        }
+    }
+}
