19cd000f52812425de180804f699b57d42398e52
rs095 <r.schwaighofer@hotmail.com>
2016-05-06 12:43:19 +0200

    vote_student_form

--- JustVote/Mapper/voting_manager.php
+++ JustVote/Mapper/voting_manager.php
@@ -146,29 +146,5 @@ class voting_manager extends manager
 
 
 
-    public function beziehungvotingstudent ($voting_id, $student_id) //????
-    {
-
-        try {
-            $stmt = $this->pdo->prepare('
-              INSERT INTO voting_student
-                (voting_id, student_id)
-              VALUES
-                (:voting_id, :student_id)
-            ');
-            $stmt->bindParam(':voting_id', $voting_id);
-            $stmt->bindParam(':student_id', $student_id);
-
-
-
-            $stmt->execute();
-        } catch (PDOException $e) {
-            echo("Fehler! Bitten wenden Sie sich an den Administrator...<br>" . $e->getMessage() . "<br>");
-            return null;
-        }
-        return true;
-    }
-
-
 }
 ?>
--- JustVote/Mapper/voting_student_manager.php
+++ JustVote/Mapper/voting_student_manager.php
@@ -2,7 +2,7 @@
 require_once("manager.php");
 require_once("voting_student.php");
 
-class voting_manager extends manager
+class voting_student_manager extends manager
 {
     protected $pdo;
 
@@ -16,4 +16,41 @@ class voting_manager extends manager
         parent::__destruct();
     }
 
-    public function
+
+    public function GetVotingName ();
+    {
+
+
+
+    }
+
+
+
+
+    /*public function beziehungvotingstudent ($voting_id, $student_id)
+    {
+
+        try {
+            $stmt = $this->pdo->prepare('
+              INSERT INTO voting_student
+                (voting_id, student_id)
+              VALUES
+                (:voting_id, :student_id)
+            ');
+            $stmt->bindParam(':voting_id', $voting_id);
+            $stmt->bindParam(':student_id', $student_id);
+
+
+
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo("Fehler! Bitten wenden Sie sich an den Administrator...<br>" . $e->getMessage() . "<br>");
+            return null;
+        }
+        return true;
+    }*/
+
+
+
+}
+?>
