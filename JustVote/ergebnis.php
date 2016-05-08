6094d9086bb4761848aea4fc7208386551194bb9
rs095 <r.schwaighofer@hotmail.com>
2016-05-08 18:05:11 +0200

    qr code

--- JustVote/ergebnis.php
+++ JustVote/ergebnis.php
@@ -39,23 +39,27 @@
                 for ($i=1; $i<=$_SESSION ["anzahlantworten"]; $i++)
                 {
 
-                // Objekt von result_manager erzeugen, welcher Datenbankverbindung besitzt
-                $ergebnismanager2 =new result_manager();
-                // lese Teilnehmeranzahl mit voting-ID aus Datenbank aus
-                $results2 = $ergebnismanager2->findByErgebnis($_SESSION["votingid"], "antwort_".$i);
+                        // Objekt von result_manager erzeugen, welcher Datenbankverbindung besitzt
+                        $ergebnismanager2 =new result_manager();
+                        // lese Teilnehmeranzahl mit voting-ID aus Datenbank aus
+                        $results2 = $ergebnismanager2->findByErgebnis($_SESSION["votingid"], "antwort_".$i);
 
-                if($results2==null)
-                {
-                    echo '<h2>Kein Datensatz wurde gefunden</h2>';
+                        if($results2==null)
+                        {
+                            echo '<h2>Kein Datensatz wurde gefunden</h2>';
+                        }
+
+                        foreach($results2 as $result2)
+                        {
+                           // array, speichert anzahl der antworten rein, minus eins weil: array fängt bei null an, unsere schleife fängt bei 1 an, aber müssen bei null anfangen wegen array
+                            $countAntwort[$i-1] = $result2->Antwort;
+                            echo '<h2> Teilnehmeranzahl: f&uumlr Antwort '.$i. ':    '.$result2->Antwort.'</h2>';
+                        }
                 }
 
-                //Schleife hier eigentlich nicht noetig, da id eindeutig und nur ein Datensatz zurueckgegeben wird
-                foreach($results2 as $result2)
-                {
-                   // echo '<h2>Kein Datensatz wurde gefunden'.$_SESSION["votingid"].'</h2>';
-                    echo '<h2> Teilnehmeranzahl: f&uumlr Antwort '.$i. ':    '.$result2->Antwort.'</h2>';
-                }
-                }
+                echo '<h2>'.$countAntwort[1].'</h2>';
+                echo '<h2>'.$countAntwort[2].'</h2>'
+
                 ?>
 
             </div>
