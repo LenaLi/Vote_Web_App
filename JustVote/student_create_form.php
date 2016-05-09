ff24e8948aadb492d42448bc8d649811b5e3ec7f
LenaLi <ll033@hdm-stuttgart.de>
2016-05-09 12:08:14 +0200

    css

--- JustVote/student_create_form.php
+++ JustVote/student_create_form.php
@@ -25,38 +25,38 @@
                 <form class="form-horizontal" role="form" action="student_create_do.php" method="post">
 
                     <div class="form-group">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <input type="text" class="form-control" name="vorname" id="vorname" placeholder="Vorname">
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <input type="text" class="form-control" name="nachname" id="nachname" placeholder="Nachname">
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <input type="text" class="form-control" name="email" id="email" placeholder="E-Mail">
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <input type="password" class="form-control" name="password" id="password" placeholder="Passwort">
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <input type="password" class="form-control" name="password2" id="password2" placeholder="Passwort Wiederholung">
                         </div>
                     </div>
 
 
                     <div class="form-group">
-                        <div class=" col-sm-6">
+                        <div class=" col-sm-6" align="right">
                             <button type="submit" class="btn btn-warning"> Anmelden</button>
                         </div>
                     </div>
