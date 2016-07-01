edc2c14686a8488f6d0e62ecabffe4e778121d4c
rs095 <r.schwaighofer@hotmail.com>
2016-07-01 19:00:52 +0200

    voting

--- JustVote/link_fuer_studenten.php
+++ JustVote/link_fuer_studenten.php
@@ -45,22 +45,28 @@ require_once("Mapper/voting.php");
 
 </div>
 
+<div>
+<?php
+    echo "https://chart.googleapis.com/chart?cht=qrchl=https://mars.iuk.hdm-stuttgart.de/~cm102/JustVote/vote_student_form.php?id='.$aktuellesvoting.=UTF-8"
+?>
+</div>
+
+<a href="http://code.google.com/apis/chart/infographics/docs/qr_codes.html">Google Chart Tools: QR Codes</a>
+<a href="http://code.google.com/p/zxing/">Google ZXing</a>
+<a href="http://zxing.appspot.com/generator/">Google ZXing QR Code Generator</a>
+
+
+
+//So muss der Link Aussehen
+http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=L&chl=https://mars.iuk.hdm-stuttgart.de/~cm102/JustVote/$aktuellesvoing>
 
 </body>
 </html>
 
-<!--
+
 // TODO QR Code https://developers.google.com/chart/infographics/docs/qr_codes#syntax
 
-/*
-<a href="http://code.google.com/apis/chart/infographics/docs/qr_codes.html">Google Chart Tools: QR Codes</a>
-<a href="http://code.google.com/p/zxing/">Google ZXing</a>
-<a href="http://zxing.appspot.com/generator/">Google ZXing QR Code Generator</a>
-*/
 
-
-//So muss der Link Aussehen
-// http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=L&chl=https://mars.iuk.hdm-stuttgart.de/~cm102/JustVote/$aktuellesvoing
 
 
 
