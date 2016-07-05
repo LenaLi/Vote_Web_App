edeef05672f4e96d013ba9ba6d3005a0aa9e274b
LenaLi <ll033@hdm-stuttgart.de>
2016-07-05 10:26:36 +0200

    bissle Code schön

--- JustVote/benutzer_password_update_form.php
+++ JustVote/benutzer_password_update_form.php
@@ -25,58 +25,57 @@ $benutzer = $benutzer_manager->findById($id);
 
 <?php include("inc/navigation.php"); ?>
 
-    <div class="container-fluid">
-            <div class="col-lg-12">
+<div class="container-fluid">
+    <div class="col-lg-12">
 
-                <!-- Beginn Formular "Passwort aktualisieren" -->
-                <h2>Passwort aktualisieren</h2>
+        <!-- Beginn Formular "Passwort aktualisieren" -->
+        <h2>Passwort aktualisieren</h2>
 
-                <?php
-                // Fehlermeldung, da Problem bei serverseitiger Überprüfung
-                // Fehlermeldung anzeigen, wenn Error Parameter mitgeliefert wird
-                if ($_GET["error"] == "1") {
+        <?php
+        // Fehlermeldung, da Problem bei serverseitiger Überprüfung
+        // Fehlermeldung anzeigen, wenn Error Parameter mitgeliefert wird
+        if ($_GET["error"] == "1") {
 
-                    // HTML Code für Fehlermeldung erzeugen
-                    echo "<div class='alert alert-danger'> <strong> Fehler: </strong>Bitte alle Felder ausfüllen </div>";
-                }
-                ?>
+            // HTML Code für Fehlermeldung erzeugen
+            echo "<div class='alert alert-danger'> <strong> Fehler: </strong>Bitte alle Felder ausfüllen </div>";
+        }
+        ?>
 
-                <!-- Beginn Formular "Passwort ändern" -->
-                <form class="form-horizontal" role="form" action="benutzer_password_update_do.php" method="post">
+        <!-- Beginn Formular "Passwort ändern" -->
+        <form class="form-horizontal" role="form" action="benutzer_password_update_do.php" method="post">
 
-                    <!-- Übergabe der Benutzer ID-->
-                    <div class="form-group">
-                        <div class="col-sm-6">
-                            <input type='hidden' name='id' value='<?php echo $benutzer->id ?>'/>
-                        </div>
-                    </div>
-
-                    <!-- Eingabe neues Passwort-->
-                    <div class="form-group">
-                        <div class="col-sm-6">
-                            <label for="password1">Neues Passwort eingeben:</label>
-                            <input class=form-control type='password' name='password1'>
-                        </div>
-                    </div>
-
-                    <!-- Neues Passwort wiederholen-->
-                    <div class="form-group">
-                        <div class="col-sm-6">
-                            <label for="password2">Passwort wiederholen:</label>
-                            <input class=form-control type='password' name='password2'>
-                        </div>
-                    </div>
-
-                    <!-- Button "Passwort ändern"-->
-                    <div class="form-group">
-                        <div class=" col-sm-6">
-                            <button type="submit" class="btn btn-warning"> Passwort ändern</button>
-                        </div>
-                    </div>
-                    <!-- Ende Formular "Passwort aktualisieren" -->
-                </form>
+            <!-- Übergabe der Benutzer ID-->
+            <div class="form-group">
+                <div class="col-sm-6">
+                    <input type='hidden' name='id' value='<?php echo $benutzer->id ?>'/>
+                </div>
             </div>
-    </div>
 
+            <!-- Eingabe neues Passwort-->
+            <div class="form-group">
+                <div class="col-sm-6">
+                    <label for="password1">Neues Passwort eingeben:</label>
+                    <input class=form-control type='password' name='password1'>
+                </div>
+            </div>
+
+            <!-- Neues Passwort wiederholen-->
+            <div class="form-group">
+                <div class="col-sm-6">
+                    <label for="password2">Passwort wiederholen:</label>
+                    <input class=form-control type='password' name='password2'>
+                </div>
+            </div>
+
+            <!-- Button "Passwort ändern"-->
+            <div class="form-group">
+                <div class=" col-sm-6">
+                    <button type="submit" class="btn btn-warning"> Passwort ändern</button>
+                </div>
+            </div>
+            <!-- Ende Formular "Passwort aktualisieren" -->
+        </form>
+    </div>  <!--"container-fluid" -->
+</div> <!--"col-lg-12" -->
 </body>
 </html>
